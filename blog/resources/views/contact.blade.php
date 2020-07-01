6cf47704734dcf51c4f66fb04ce5e27770276a40
Jean-PierrePapin <skouo82@gmail.com>
2020-07-01 08:56:09 +0100

    Layouts setup

--- blog/resources/views/contact.blade.php
+++ blog/resources/views/contact.blade.php
@@ -1,33 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <h1>Contact Page</h1>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="container">
-                <h1>Contact Page</h1>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
