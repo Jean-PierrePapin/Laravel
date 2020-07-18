d6b3c1a37bd0abeef6b66ddd4393b4779859ca72
Jean-PierrePapin <skouo82@gmail.com>
2020-07-18 19:22:03 +0100

    relation and mass assignment

--- onetomany/app/User.php
+++ onetomany/app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Post relation
+     */
+
+    public function posts() {
+
+        return $this->hasMany('App\Post');
+
+    }
 }
