96e6825e92da40c2c6ec2d8b7de5011d47c86809
Jean-PierrePapin <skouo82@gmail.com>
2020-07-25 10:00:32 +0100

    Relationships and mass assignment, inserting reading and updating data

--- polymorphicmanytomany/routes/web.php
+++ polymorphicmanytomany/routes/web.php
@@ -13,8 +13,70 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Post;
+use App\Tag;
+use App\Video;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+/**
+ * Inserting Data
+ */
+
+Route::get('/create', function() {
+
+    $post = Post::create(['name'=>'My first post']);
+
+    $tag1 = Tag::find(1);
+
+    $post->tags()->save($tag1);
+
+    $video = Video::create(['name'=>'video.mov']);
+
+    $tag2 = Tag::find(2);
+
+    $video->tags()->save($tag2);
+
+});
+
+/**
+ * Reading and updating data
+ */
+
+Route::get('/read', function() {
+
+    $post = Post::findOrFail(4);
+
+    foreach($post->tags as $tag) {
+
+        echo $tag;
+
+    }
+
+});
+
+Route::get('/update', function() {
+
+    /* $post = Post::findOrFail(4);
+
+    foreach($post->tags as $tag) {
+
+        return $tag->whereName('PHP')->update(['name'=>'Updated PHP']);
+
+    }
+ */
+
+    $post = Post::findOrFail(1);
+
+    $tag = Tag::find(3);
+
+    // $post->tags()->save($tag);
+
+    // $post->tags()->attach($tag);
+
+    $post->tags()->sync([2]);
+
+});
