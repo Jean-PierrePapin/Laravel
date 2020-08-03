47b92d67245e2cc4316020b21d530ef35fe83593
Jean-PierrePapin <skouo82@gmail.com>
2020-08-03 17:46:37 +0100

    Advance validation

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
 use App\Post;
 use Illuminate\Http\Request;
 
@@ -40,16 +41,16 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostRequest $request)
     {
         //
 
-        $this->validate($request, [
+        /* $this->validate($request, [
 
             'title'     => 'required',
             'content'   => 'required'
 
-        ]);
+        ]); */
 
 
         /**
