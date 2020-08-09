4156e5ca39fcb64868c7a05141e47098ec047798
Jean-PierrePapin <skouo82@gmail.com>
2020-08-09 10:03:39 +0100

    Retrieving authenticated user data

--- login/routes/web.php
+++ login/routes/web.php
@@ -12,9 +12,39 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    // return view('welcome');
+
+    /**
+     * If condition checks if the user is logged in
+     */
+
+    /* if(Auth::check()) {
+
+        return "the user is logged in";
+
+    }
+ */
+    /**
+     * Redirect the user on the admin page and typing the ($username, $password) variables 
+     * in order to define them
+     */
+
+    /* $username = 's<g<zshr';
+    $password = 'ehtrhg564186';
+
+    if(Auth::attempt(['username' => $username, 'password' => $password])) {
+
+        return redirect()->intended('/admin');
+
+    } */
+    
+    Auth::logout();
+
 });
 
 Auth::routes();
