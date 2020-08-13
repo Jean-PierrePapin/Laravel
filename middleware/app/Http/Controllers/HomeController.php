d526748994fb5eb50d6b86effeb7b936a28a1ceb
Jean-PierrePapin <skouo82@gmail.com>
2020-08-13 09:05:20 +0100

    flashing data

--- middleware/app/Http/Controllers/HomeController.php
+++ middleware/app/Http/Controllers/HomeController.php
@@ -28,25 +28,44 @@ class HomeController extends Controller
 
         // $request->session()->get('Samuel);
 
-
+        /**
+         * Add a session
+         */
         //session(['Samuel2' => 'Your freelancer']);
 
         //return session('Samuel2');
 
+        /**
+         * Deleting one session or all of them
+         */
+
         //$request->session()->forget('Samuel2');
 
 
-        $request->session()->flush();
-
-        return $request->session()->all();
-
-
+        //$request->session()->flush();
 
         //return $request->session()->all();
 
+        //return $request->session()->all();
+
+        /**
+         * Get a session
+         */
+
         //echo $request->session()->get('Samuel');
 
-        
+        /**
+         * Flashing data
+         */        
+
+        //$request->session()->flash('message', 'Post has been created');
+
+        //return $request->session()->get('message');
+
+        $request->session()->reflash();
+
+        $request->session()->keep()'message';
+
 
         //return view('home');
     }
