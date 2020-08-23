25773579a270256b5766d8ec03f0f2bedd6056d3
Jean-PierrePapin <skouo82@gmail.com>
2020-08-23 10:10:58 +0100

    Done again the admin view creation setuip

--- new-cms-system/resources/views/components/admin-master.blade.php
+++ new-cms-system/resources/views/components/admin-master.blade.php
@@ -333,8 +333,8 @@
             <!-- Begin Page Content -->
             <div class="container-fluid">
 
-            <!-- Page Heading -->
-            @yield('content')
+                <!-- Page Heading -->
+                @yield('content')
 
             </div>
             <!-- /.container-fluid -->
