2cc001b6adfbf14ae56defd20905fa00d72b9a05
Jean-PierrePapin <skouo82@gmail.com>
2020-09-19 09:15:28 +0100

    Routes for large applications

---
+++ new-cms-system/routes/web/users.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::put('/users/{user}/update', 'UserController@update')->name('user.profile.update');
+    
+Route::delete('/users/{user}/destroy', 'UserController@destroy')->name('users.destroy');
+
+Route::middleware(['role:admin', 'auth'])->group(function(){
+
+    Route::get('/users', 'UserController@index')->name('users.index');
+
+});
+
+Route::middleware(['can:view,user'])->group(function(){
+
+    Route::get('/users/{user}/profile', 'UserController@show')->name('user.profile.show');
+
+});
