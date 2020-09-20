58625bebc5092593da7aff6fe5deef2b7bf68491
Jean-PierrePapin <skouo82@gmail.com>
2020-09-20 09:25:25 +0100

    Detecting the user role

--- new-cms-system/resources/views/admin/users/profile.blade.php
+++ new-cms-system/resources/views/admin/users/profile.blade.php
@@ -132,7 +132,13 @@
                                 <tbody>
                                     @foreach($roles as $role)
                                         <tr>
-                                            <td><input type="checkbox"></td>
+                                            <td><input type="checkbox"
+                                                        @foreach($user->roles as $user_role)
+                                                            @if($user_role->slug == $role->slug)
+                                                                checked
+                                                            @endif
+                                                        @endforeach
+                                                ></td>
                                             <td>{{ $role->id }}</td>
                                             <td>{{ $role->name }}</td>
                                             <td>{{ $role->slug }}</td>
