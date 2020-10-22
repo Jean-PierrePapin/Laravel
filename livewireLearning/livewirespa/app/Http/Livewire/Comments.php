98200c82a66f9209131861f2f0b3e2dbb22c4e8a
Jean-PierrePapin <skouo82@gmail.com>
2020-10-22 17:27:27 +0100

    Class AppHttpLivewireCarbon not found

--- livewireLearning/livewirespa/app/Http/Livewire/Comments.php
+++ livewireLearning/livewirespa/app/Http/Livewire/Comments.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Livewire;
 
+// namespace App\Http\Livewire\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
+    public $comments = [
+        [
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis eget purus ut aliquam. Curabitur feugiat eros nec augue vulputate, eget tempus lorem rhoncus. Proin lobortis at dolor ut semper. Maecenas bibendum tincidunt elit et pellentesque. In fermentum pulvinar scelerisque. Fusce euismod elit felis, euismod feugiat nunc elementum vitae. Donec egestas ullamcorper purus, non consectetur magna dapibus a. Aenean et nisl sit amet enim pellentesque tincidunt vel in dolor. Nullam pretium orci pulvinar mi tempus, non consectetur odio placerat. Etiam cursus tortor velit, sed facilisis neque sollicitudin sit amet. Ut in rutrum velit.',
+            'created_at' => '3 min ago',
+            'creator' => 'Starhak'
+        ]
+    ];
+
+    public $newComment;
+
+    public function addComment() 
+    {
+        array_unshift($this->comments, [
+            'body' => $this->newComment,
+            'created_at' => Carbon::now()->diffForHumans(),
+            'creator' => 'Bibi'
+        ]);
+
+    }
+
     public function render()
     {
         return view('livewire.comments');
